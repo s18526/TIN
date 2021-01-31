4d8eff74ef9c05050c9711f732f5b4644ba0641e
s18526 <61704163+s18526@users.noreply.github.com>
2021-01-31 18:01:32 +0100

    Add files via upload

---
+++ lab7/7b.js
@@ -0,0 +1,35 @@
+const http = require("http");
+const fs = require("fs");
+
+const PORT = 8000;
+const PATH = "./";
+let watching = false;
+http
+    .createServer((req, res) => {
+        res.end("Waiting for requests...");
+        fs.watch(PATH, (eventType, filename) => {
+            if (watching) return;
+            watching = true;
+
+            try {
+                readFile(filename);
+            } catch (error) {
+                res.writeHead(404, {"Content-Type": "text/plain"});
+                res.end("Server error");
+            }
+
+            setTimeout(() => {
+                watching = false;
+            }, 100);
+        });
+    })
+    .listen(PORT);
+
+const readFile = (filename) => {
+    fs.readFile(filename, (err, data) => {
+        if (err) throw err;
+        console.log(data.toString());
+    });
+};
+
+console.log("server is running on port: " + PORT);
